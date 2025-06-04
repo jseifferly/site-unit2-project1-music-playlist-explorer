c015a1ececf82a4bd4cfe09724eba0025aae61ba
Jackson Kenneth Seifferly <jseifferly@meta.com>
2025-06-04 14:40:01 -0700

    Got modal popping up and closing properly but the window is not formatted correctly

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,15 @@
+const modal = document.getElementById("modal");
+const close_button = document.getElementsByClassName("close")[0];
+let playlist = document.querySelector(".playlist");
 
+function openModal(playlist) {
+    modal.style.display = "block"
+}
+
+close_button.addEventListener("click", () => {
+    modal.style.display = "none"
+})
+
+playlist.addEventListener("click", () => {
+    openModal()
+})
